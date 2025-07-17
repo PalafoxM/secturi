1260f1717a43b2fcc27a6d6fa1675da50c99a429
PalafoxM <palafox.marin31@gmail.com>
2025-07-17 13:50:17 -0600

    ubicacion

--- js/principal.js
+++ js/principal.js
@@ -126,9 +126,86 @@ saeg.principal = (function () {
                     // Error al obtener ubicación
                     $('#btn_login').show();
                     $('#btn_load').hide();
+                    
+                    Swal.fire({
+                    title: "Ubicación requerida",
+                    text: "SUSI pide su ubicacion para la asistencia",
+                    icon: "info",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Seguir sin ubicacion"
+                    }).then((result) => {
+                    if (result.isConfirmed) {
+                        saeg.principal.loginSinUbicacion();
+                    }
+                    });
 
-                    Swal.fire("Ubicación requerida", "Debe permitir el acceso a su ubicación para continuar", "error");
+                  
                 });
+            },
+            loginSinUbicacion: function() {
+                let usuario = $('#usuario').val();              
+                let contrasenia = $('#contrasenia').val(); 
+        
+                if (!usuario || !contrasenia) {
+                    Swal.fire("¡Atención!", "Es requerido el usuario y contraseña", "error");
+                    return;
+                }
+                $('#btn_login').hide();           
+                $('#btn_load').show(); 
+                    $.ajax({
+                        type: "POST",
+                        url: base_url + "index.php/Login/validar_usuario",
+                        data: {
+                            usuario,
+                            contrasenia
+                        },
+                        dataType: "json",
+                        success: function(response) {
+                            console.log(response);
+                            if (!response.error) {
+                                Swal.fire("Bienvenido!", "Ingresando...", "success");
+                                if(response.asistencia){
+                                    setTimeout(() => {
+                                        Swal.fire({
+                                            position: "top-end",
+                                            icon: "success",
+                                            title: "Asistencia registrada automáticamente",
+                                            showConfirmButton: false,
+                                            timer: 1000
+                                        });
+                                    }, 1000);
+                                }else{
+                                    setTimeout(() => {
+                                        Swal.fire({
+                                            position: "top-end",
+                                            icon: "error",
+                                            title: "Ubicación fuera de rango, asistencia no registrada",
+                                            showConfirmButton: false,
+                                            timer: 2500
+                                        });
+                                    }, 1000);
+                                }
+
+                                setTimeout(() => {
+                                    window.location.href = base_url + "index.php/Inicio";
+                                }, 3000);
+                            } else {
+                                Swal.fire("Usuario incorrecto", "Favor de verificar sus credenciales", "error");
+                            }
+                        },
+                        complete: function () {
+                            $('#btn_login').show();
+                            $('#btn_load').hide();
+                        },
+                        error: function(jqXHR, textStatus, errorThrown) {
+                            Swal.fire("Error en la conexión", textStatus, "error");
+                            console.error('Error:', textStatus, errorThrown);
+                        }
+                    });
+
+           
             },
      
 
