5b1bb4b66c064f4391b69112a6eb9f9ac25a5c2a
PalafoxM <palafox.marin31@gmail.com>
2025-08-04 13:13:31 -0600

    query 2 seccion de jefes

--- app/Controllers/Principal.php
+++ app/Controllers/Principal.php
@@ -1333,7 +1333,7 @@ class Principal extends BaseController {
         $globals      = new Mglobal;
         $incidencia    = $globals->getTabla(['tabla' => 'vw_incidenica', 'where' => ['visible' => 1, 'id_jefe_inmediato' => $session->get('id_usuario')]]);
          $Periodo= ['tabla' => 'cat_periodo', 'where' => ['visible' => 1]];
-      $usuario = [
+       $usuario = [
             'tabla'   => 'vw_incidenica',
             'select'  => ['id_usuario', 'nombre_completo'],
             'where'   => ['visible' => 1],
