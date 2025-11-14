fb057b0949039ea558a88bace1db468b1c662e9e
PalafoxM <palafox.marin31@gmail.com>
2025-11-14 09:21:16 -0600

    banco

--- app/Views/secciones/vFormatoVI.php
+++ app/Views/secciones/vFormatoVI.php
@@ -31,10 +31,10 @@
             <span class="proxima "><strong>NO. CUENTA : <?= isset($proveedorBanco->no_cuenta) && !empty($proveedorBanco->no_cuenta)?$proveedorBanco->no_cuenta:'' ?> </span>
         </div>
         <div  style="position:absolute; top:43%; left:60.8%; width:36%; background-color:white; font-size: 12px;  height:12px;">
-            <span class="proxima "><STRONG>BANCO: </STRONG><?= $proveedorBanco->banco?></span>
+            <span class="proxima "><STRONG>BANCO: </STRONG><?= isset($proveedorBanco->banco) && !empty($proveedorBanco->banco)?$proveedorBanco->banco:'' ?></span>
         </div>
          <div  style="position:absolute; top:44.3%; left:60.8%; width:36%; background-color:white; font-size: 12px;  height:12px;">
-            <span class="proxima "><STRONG>CLABE:</STRONG> <?= $proveedorBanco->clabe ?></span>
+            <span class="proxima "><STRONG>CLABE:</STRONG> <?= isset($proveedorBanco->clabe) && !empty($proveedorBanco->clabe)?$proveedorBanco->clabe:'' ?></span>
         </div>
          <div  style="position:absolute; top:45.3%; left:60.8%; width:35%; background-color:white; font-size: 12px;  height:15px;">
             <span class="proxima "></span>
