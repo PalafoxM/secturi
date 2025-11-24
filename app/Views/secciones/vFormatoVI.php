ba78678d4d13b208e37ab13c00d91ec53cf19516
PalafoxM <palafox.marin31@gmail.com>
2025-11-24 10:06:58 -0600

    cambio

--- app/Views/secciones/vFormatoVI.php
+++ app/Views/secciones/vFormatoVI.php
@@ -51,7 +51,7 @@
                        <?= number_format($vehiculo->xml_monto, 2)  ?>
                     </span>
               </div>
-              <div style="position:absolute; text-align:center; top:34%; left:2.7%; width:13.8%; background-color:white; font-size: 12px;  height:12px;">
+              <div style="position:absolute; text-align:center; top:34%; left:2.7%; width:13.8%; background-color:white; font-size: 12px;  height:18px;">
                   <p> <?= $vehiculo->xml_uuid ?> </p>
                 </div>
     
