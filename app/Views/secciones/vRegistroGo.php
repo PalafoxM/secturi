183ee6b50351e1bceb7bfc941616842c309d28de
APL <palafox.marin31@gmail.com>
2025-12-24 14:19:55 -0600

    se agrega el readonly

--- app/Views/secciones/vRegistroGo.php
+++ app/Views/secciones/vRegistroGo.php
@@ -236,7 +236,7 @@
                                                 style="color:red;">*</span></label>
                                         <input type="number" class="form-control" autocomplete="off" id="no_consecutivo"
                                             name="no_consecutivo" placeholder="001"
-                                            value="<?= (isset($no_consecutivo)) ? $no_consecutivo : '' ?>">
+                                            value="<?= (isset($no_consecutivo)) ? $no_consecutivo : '' ?>" readonly>
                                         <div class="invalid-feedback">
                                             Campo no Valido
                                         </div>
