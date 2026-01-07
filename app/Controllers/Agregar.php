a3d663ed544c883dc6d1b306d25ed97d27e01654
PalafoxM <palafox.marin31@gmail.com>
2026-01-07 08:31:50 -0600

    justificaciones

--- app/Controllers/Agregar.php
+++ app/Controllers/Agregar.php
@@ -3794,9 +3794,26 @@ class Agregar extends BaseController
             $anio . '-05-01' => 'Día del Trabajo',
             $anio . '-09-16' => 'Día de la Independencia',
             $anio . '-11-17' => 'Asueto',
-            $anio . '-12-12' => 'Día de la Virgen de Guadalupe',
+           '2025-12-12' => 'Día de la Virgen de Guadalupe',
             $anio . '-12-25' => 'Navidad',
-            $anio . '-' . $cumple => 'Mi cumpleaños'
+            $anio . '-' . $cumple => 'Mi cumpleaños',
+            '2025-12-22' => '2do Periodo Vacacional',
+            '2025-12-23' => '2do Periodo Vacacional',
+            '2025-12-24' => '2do Periodo Vacacional',
+            '2025-12-25' => '2do Periodo Vacacional',
+            '2025-12-26' => '2do Periodo Vacacional',
+            '2025-12-27' => '2do Periodo Vacacional',
+            '2025-12-28' => '2do Periodo Vacacional',
+            '2025-12-29' => '2do Periodo Vacacional',
+            '2025-12-30' => '2do Periodo Vacacional',
+            '2025-12-31' => '2do Periodo Vacacional',
+            $anio . '-01-01' => '2do Periodo Vacacional',
+            $anio . '-01-02' => '2do Periodo Vacacional',
+            $anio . '-01-03' => '2do Periodo Vacacional',
+            $anio . '-01-04' => '2do Periodo Vacacional',
+            $anio . '-01-05' => '2do Periodo Vacacional',
+            $anio . '-01-06' => '2do Periodo Vacacional',
+    
         ];
 
         $data['diasFestivos'] = $diasFestivos;
