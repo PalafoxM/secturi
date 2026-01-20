8240097038115fc92bd0eb5eb55ec88d3ec58a30
PalafoxM <palafox.marin31@gmail.com>
2026-01-20 11:45:06 -0600

    f

--- app/Views/secciones/vFormatoGO.php
+++ app/Views/secciones/vFormatoGO.php
@@ -36,11 +36,11 @@
                }
             ?>
          <div  style="position:absolute; text-align:center; top:<?=$i?>%; left:22%; width:10%; background-color:white; font-size: 10px;  height:12px;">
-            <span class="proxima "><?= $r->dsc_partida ?></span>
+            <span class="proxima "><?= $r->dsc_proyecto ?></span>
         </div>
 
          <div  style="position:absolute; text-align:center; top:<?=$i?>%; left:33%; width:16%; background-color:white; font-size: 10px;  height:12px;">
-            <span class="proxima "><?= $r->dsc_proyecto ?></span>
+            <span class="proxima "><?= $r->dsc_partida ?></span>
         </div>
         <?php $i = $i + 1.5; ?>
         <?php endforeach; ?>
