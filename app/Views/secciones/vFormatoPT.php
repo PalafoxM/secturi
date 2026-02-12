79cad8b2c75267b1fc44b4c9202d8dd28eecca01
PalafoxM <palafox.marin31@gmail.com>
2026-02-12 14:55:07 -0600

    fsdfsd

--- app/Views/secciones/vFormatoPT.php
+++ app/Views/secciones/vFormatoPT.php
@@ -137,14 +137,7 @@
             <span class="proxima "><?= $usu_sub->dsc_subsecretario?></span>
         </div>
          <?php endif; ?>
-         <?php if($fic): ?>
-         <div  style="position:absolute;  text-align:center; top:67.7%; left:67.1%; width:30%; background-color:white; font-size: 12px;  height:12px;">
-            <span class="proxima">MTRO. DAVID AYALA SAUCEDO  </span>
-        </div>
-         <?php endif; ?>
-         <div  style="position:absolute;  text-align:center; top:69%; left:67.1%; width:30%; background-color:white; font-size: 12px;  height:12px;">
-            <span class="proxima ">DIRECTOR GENERAL DE DESARROLLO TURÍSTICO </span>
-        </div>
+       
          <div  style="position:absolute;  text-align:center; top:79%; left:67.1%; width:30%; background-color:white; font-size: 12px;  height:12px;">
             <span class="proxima "> <?= (isset($responsableGasto->nombre_completo) && !empty($responsableGasto->nombre_completo))?$responsableGasto->nombre_completo:'' ?> </span>
         </div>
