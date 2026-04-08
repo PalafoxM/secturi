287d536f4181455bd678da824407de162c758a0a
APL <palafox.marin31@gmail.com>
2026-04-07 19:51:34 -0600

    nuevas claves de google

--- app/Controllers/Principal.php
+++ app/Controllers/Principal.php
@@ -2187,7 +2187,7 @@ class Principal extends BaseController
         $email = \Config\Services::email();
         $result = $globals->getTabla(['tabla' => 'vw_usuario', 'where' => ['visible' => 1, 'id_tipo_empleado' => 1]])->data;
 
-       $email->setTo([
+      /* $email->setTo([
             'agascag@guanajuato.gob.mx',
             'ccampos@guanajuato.gob.mx',
             'ztorrest@guanajuato.gob.mx',
@@ -2201,9 +2201,9 @@ class Principal extends BaseController
             'rgonzalezgu@guanajuato.gob.mx',
             'yjimenez@guanajuato.gob.mx',
             'mamoralesg@guanajuato.gob.mx',
-        ]);
+        ]);*/
  
-        /*  $email->setTo([
+          $email->setTo([
                     'alopez@guanajuato.gob.mx',
                     'cchernandezp@guanajuato.gob.mx',
                     'csoto@guanajuato.gob.mx',
@@ -2243,7 +2243,7 @@ class Principal extends BaseController
                     'jmazavala@guanajuato.gob.mx',
                     'rantonio@guanajuato.gob.mx',
                     'jrodriguezgo@guanajuato.gob.mx',
-                ]);*/
+                ]);
         $email->setSubject('Recordatorio: Revisión de Asistencias - Sistema SUSI');
         $email->setMessage('
             <!DOCTYPE html>
@@ -2276,9 +2276,9 @@ class Principal extends BaseController
                         <p style="font-size: 16px; line-height: 1.6; margin-bottom: 20px;">Estimado personal,</p>
                         
                         <p style="font-size: 16px; line-height: 1.6; margin-bottom: 20px;">
-                            En caso de que aún no hayas realizado las <strong>justificaciones de tu personal correspondientes a la quincena 05/2026</strong>, 
-                            la cual comprende el periodo del <strong>01 al 15 de marzo de 2026</strong>, 
-                            tienes hasta el día <strong>Martes 24 de marzo hasta las 16:00 hrs</strong> para realizarlas.
+                            En caso de que aún no hayas realizado las <strong>justificaciones correspondientes a la quincena 06/2026</strong>, 
+                            la cual comprende el periodo del <strong>16 al 31 de marzo de 2026</strong>, 
+                            tienes hasta el día <strong>viernes 10 de abril hasta las 16:00 hrs</strong> para realizarlas.
                         </p>
 
                         <div class="highlight-box">
@@ -2290,11 +2290,11 @@ class Principal extends BaseController
                         </div>
 
                         <p style="font-size: 16px; line-height: 1.6; margin-bottom: 25px;">
-                            Le invitamos a revisar y validar las incidencias en el sistema SUSI.
+                            Le invitamos a revisar las incidencias en el sistema SUSI.
                         </p>
 
                         <div style="text-align: center; margin: 30px 0;">
-                            <a href="https://secturnet.guanajuato.gob.mx/susi/index.php/Principal/incidenciaSubordinado" class="btn" style="color: white; text-decoration: none;">
+                            <a href="https://secturnet.guanajuato.gob.mx/susi/index.php/Agregar/Asistencia" class="btn" style="color: white; text-decoration: none;">
                                 📋 Revisar Incidencias
                             </a>
                         </div>
@@ -6838,7 +6838,8 @@ class Principal extends BaseController
                 }
             }
         }
-
+       
+        //die( var_dump( $data['id_area'] ) );
    
         
         $data['scripts'] = array('principal');
