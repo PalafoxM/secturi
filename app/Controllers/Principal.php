19bb2f6c62c199557a3ad1688253f078cbc83b51
PalafoxM <palafox.marin31@gmail.com>
2026-04-14 12:38:05 -0600

    sasas

--- app/Controllers/Principal.php
+++ app/Controllers/Principal.php
@@ -9265,7 +9265,7 @@ class Principal extends BaseController
         $data['pagos'] = (!empty($pagos->data)) ? $pagos->data : [];
         
         // Similar to Contrato PDF View
-        $html = view('pdfs/vpdfFicha', $data);
+        $html = view('personal/vFicha', $data);
         
         $mpdf = new \Mpdf\Mpdf([
             'margin_top' => 10,
