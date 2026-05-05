f8128beedf8d1160de7cc2c06ad6196e7e172860
Marin <palafox.marin31@gmail.com>
2026-05-05 13:03:28 -0600

    CORREO

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -38,7 +38,8 @@ class Email extends BaseConfig
     /**
      * SMTP Password
      */
-    public string $SMTPPass = 'fohs cwuv udom uajp';
+    public string $SMTPPass = 'kfny yyit wpfe vraa';
+    //public string $SMTPPass = 'fohs cwuv udom uajp';
     //public string $SMTPPass = 'kaej sezv flls fuzk';
 
     /**
