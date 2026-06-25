097b1fac1c7aa06e71dfea9b0c9a83d5a7a59041
PalafoxM <a.palfoxm@guanajuato.gob.mx>
2026-06-25 13:01:30 -0600

    Cambios locales

--- app/Libraries/Funciones.php
+++ app/Libraries/Funciones.php
@@ -10,7 +10,7 @@ class Funciones {
     public function __construct()
     {
         $this->globals = new Globals();
-        $this->secretKey = 'ORnsLEykJAMTEvacurIPAMAeRvelINclOg';
+        $this->secretKey = env('TOKEN_API') ?: 'ORnsLEykJAMTEvacurICINGeRvelINclOg';
     }
 
     function encode($data) {
